9bdb85ed9d94e2b8f9e34ce9d49676e55c7a1b09
sanish <notsansh@outlook.com>
2024-09-17 16:19:32 +0545

    styling components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Contact from './components/Contact';
+import './styles/app.css';
 
 const App = () => {
   const contacts = [
@@ -10,9 +11,13 @@ const App = () => {
     { name: 'Sanish', number: '2222', location: 'Australia' },
     { name: 'Ram', number: '3333', location: 'USA' },
   ];
+
+  // Dynamically pani color set garna milxa
+  const loggedIn = true;
+
   return (
     <>
-      <div className='container'>
+      <div className='container' style={{ color: loggedIn ? 'green' : 'red' }}>
         <h3>Contact List:</h3>
 
         <Contact data={contacts[0]} />
