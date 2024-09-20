3d7f87a9f286a8c5478226bed6b2f5abd251990f
sanish <notsansh@outlook.com>
2024-09-20 13:24:23 +0545

    Intro to state management

--- src/components/ContactAdder.jsx
+++ src/components/ContactAdder.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ContactAdder = () => {
+  const [name, setName] = useState('John');
+
   const onClickHandler = () => {
-    console.log('Clicked');
+    setName('Dave');
+    console.log(name);
   };
+
   return (
     <>
       <div className='simpleAdder'>
         Contact Adder:
         <button onClick={onClickHandler}>Click me!</button>
+        <div>My name is {name}.{name==="Dave" ? "I live in Nepal" :""}</div>
       </div>
     </>
   );
