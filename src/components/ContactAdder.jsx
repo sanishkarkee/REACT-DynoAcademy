b6507514cac5d82258ff76aac09ed99e37a18e82
sanish <notsansh@outlook.com>
2024-09-23 13:03:05 +0545

    using useState to store user input data & displaying them

--- src/components/ContactAdder.jsx
+++ src/components/ContactAdder.jsx
@@ -1,31 +1,48 @@
-import React, { useState } from 'react';
-import { Chris, Dave, John } from './Persons';
+import { useState } from 'react';
 
 const ContactAdder = () => {
-  const [name, setName] = useState('John');
+  const [name, setName] = useState('');
+  const [mobile, setMobile] = useState('');
+  const [location, setLocation] = useState('');
 
   const onClickHandler = () => {
-    setName('Chris');
-    console.log(name);
+    console.log('Clicked');
   };
-
-  let resultsCard;
-  if (name === 'Dave') {
-    resultsCard = <Dave />;
-  } else if (name === 'John') {
-    resultsCard = <John />;
-  } else if (name === 'Chris') {
-    resultsCard = <Chris />;
-  }
 
   return (
     <>
       <div className='simpleAdder'>
-        Contact Adder:
+        Contact Adder: <br />
+        <input
+          type='text'
+          value={name}
+          placeholder='Contact name'
+          onChange={(e) => {
+            console.log(e.target.value);
+            setName(e.target.value);
+          }}
+        />
+        <input
+          type='text'
+          value={mobile}
+          placeholder='Mobile'
+          onChange={(e) => {
+            console.log(e.target.value);
+            setMobile(e.target.value);
+          }}
+        />
+        <input
+          type='location'
+          value={location}
+          placeholder='Location'
+          onChange={(e) => {
+            console.log(e.target.value);
+            setLocation(e.target.value);
+          }}
+        />
+        <br />
+        <br />
         <button onClick={onClickHandler}>Click me!</button>
-        <div>
-          My name is {name}.{resultsCard}
-        </div>
       </div>
     </>
   );
