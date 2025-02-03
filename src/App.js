bf6e4774bb3eb4bd433fcd32f286aae933544178
sanish <s.karki1994@gmail.com>
2025-02-03 15:03:04 +0545

    Try-catch to handle error and displying custom message after error is encountered

--- src/App.js
+++ src/App.js
@@ -1,41 +1,39 @@
 import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
   // 3) -----ASYNCS-AWAIT
+
+  const [movies, setMovies] = useState([]);
+
   const fetchMovies = async () => {
     // 1)- Fetch resource...
     console.log('Calling API');
-    const response = await axios.get(
-      'https://api.dynoacademy.com/test-api/v1/movies'
-    );
-    console.log(response);
-    console.log('Finish');
 
-    // 2) -----PROMISE-----
-    // const promise = new Promise((resolve, reject) => {
-    //   const response = axios.get(
-    //     'https://api.dynoacademy.com/test-api/v1/movies'
-    //   );
-    //   resolve(response);
-    // });
-    // promise
-    //   .then((result) => {
-    //     console.log(result);
-    //     console.log('Finish');
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
+    try {
+      const response = await axios.get(
+        'https://api.dynoacademy.com/test-api/v12/movies'
+      );
+      setMovies(response.data.moviesData); //Axios le jaile pani "data" bhitra data haru pathako hunxa
+    } catch (error) {
+      alert('Cannot get movies data.');
+    }
+
+    console.log(movies);
   };
 
-  return (
+  return ( 
     <div className='App'>
       <button onClick={fetchMovies}>Get all movies</button>
       <br />
 
-      <div
-        style={{ background: '#e7e7e7', padding: '10px', margin: '5px' }}
-      ></div>
+      <div style={{ background: '#e7e7e7', padding: '10px', margin: '5px' }}>
+        {movies.map((el) => (
+          <div key={el.id} style={{ marginBottom: '10px' }}>
+            {el.name}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
