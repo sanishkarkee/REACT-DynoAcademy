f8dbed2f1739f0652cca5d11e321dfac9fdad9da
sanish <s.karki1994@gmail.com>
2025-02-04 16:16:28 +0545

    single movie ko info API bata GET garera page ma display gareko

--- src/pages/ViewMovie.js
+++ src/pages/ViewMovie.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
 
 const ViewMovie = () => {
   const getParams = useParams(); // {id:'3'}
   const getId = getParams.id; //3
+  //   console.log(getId);
 
-  console.log(getId);
+  const [movieData, setMovieData] = useState({});
 
-  return <>View Movie {getId}</>;
+  const getSingleMovieInfo = async () => {
+    try {
+      // yo API bata 1 ta movie ko info aako xa so ,Yo component ma MAP garnu pardaina
+      const response = await axios.get(
+        `https://api.dynoacademy.com/test-api/v1/movie/${getId}`
+      );
+      setMovieData(response.data.singleMovieData);
+    } catch (error) {
+      alert('Error occured!');
+    }
+  };
+
+  return (
+    <>
+      View Movie {getId}
+      <button onClick={getSingleMovieInfo}>
+        View This movie Details!
+      </button>{' '}
+      <br />
+      <h2>Movie Detail:</h2>
+      <br />
+      Movie Name: {movieData.name} <br /> <br />
+      Info: {movieData.info} <br /> <br />
+      Description: {movieData.desc} <br /> <br />
+      Rating: {movieData.rating} <br /> <br />
+      Image : <br /> <br />
+      <img src={movieData.image} alt='' style={{ height: '200px' }} />
+    </>
+  );
 };
 
 export default ViewMovie;
